a02bcf8b89ec1e919c28a6949e4ea0a0c53356e5
Tino <sackvalentin@gmail.com>
2026-02-17 19:42:10 +0100

    1

--- C08/ex04/ft_stock_str.h
+++ C08/ex04/ft_stock_str.h
@@ -1,6 +1,25 @@
+/******************************************************************************/
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vsack <vsack@student.42vienna.com>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/17 19:23:09 by vsack             #+#    #+#             */
+/*   Updated: 2026/02/17 19:40:46 by vsack            ###   ########.fr       */
+/*                                                                            */
+/******************************************************************************/
+
+#ifndef FT_STOCK_STR_H
+# define FT_STOCK_STR_H
+
 typedef struct s_stock_str
 {
-int size;
-char *str;
-char *copy;
-} t_stock_str;
+	int		size;
+	char	*str;
+	char	*copy;
+}			t_stock_str;
+
+t_stock_str	*ft_strs_to_tab(int ac, char **av);
+
+#endif
