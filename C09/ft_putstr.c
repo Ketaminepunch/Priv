815f29ecaadaebc885d5626f082f92dcebee1a02
Valentin Sack <vsack@c2r1p5.42vienna.com>
2026-02-18 21:06:23 +0100

    121

--- C09/ft_putstr.c
+++ C09/ft_putstr.c
@@ -1,19 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vsack <vsack@student.42vienna.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/02/17 19:50:36 by vsack             #+#    #+#             */
-/*   Updated: 2026/02/18 14:18:00 by vsack            ###   ########.fr       */
+/*   Created: 2026/02/18 16:40:59 by vsack             #+#    #+#             */
+/*   Updated: 2026/02/18 21:05:53 by vsack            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_stock_str.h"
 #include <unistd.h>
 
-static void	ft_putstr(char *str)
+void	ft_putstr(char *str)
 {
 	int	i;
 
@@ -21,40 +20,6 @@ static void	ft_putstr(char *str)
 	while (str[i])
 	{
 		write(1, &str[i], 1);
-		i++;
-	}
-}
-
-static void	ft_putnbr(int n)
-{
-	long	num;
-	char	c;
-
-	num = n;
-	if (num < 0)
-	{
-		write(1, "-", 1);
-		num = -num;
-	}
-	if (num > 9)
-		ft_putnbr(num / 10);
-	c = (num % 10) + '0';
-	write(1, &c, 1);
-}
-
-void	ft_show_tab(struct s_stock_str *par)
-{
-	int	i;
-
-	i = 0;
-	while (par[i].str)
-	{
-		ft_putstr(par[i].str);
-		write(1, "\n", 1);
-		ft_putnbr(par[i].size);
-		write(1, "\n", 1);
-		ft_putstr(par[i].copy);
-		write(1, "\n", 1);
 		i++;
 	}
 }
